c7f1908a1ba8d714527704f5477c61ad6e6536ea
Joseph <josephpeter489@gmail.com>
2023-10-01 21:35:05 +0100

    my second commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+var corOptions = {
+    origin: 'https://localhost:8081'
+}
+
+
+// Middleware
+app.use(cors(corOptions))
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
+
+
+// Testing API
+app.get('/', (req, res)=>{
+    res.json({ 
+        message: 'Hello from API'
+    })
+})
+
+const PORT = process.env.PORT || 8080
+
+// Server
+app.listen(PORT, ()=>{
+    console.log(`Server is running on ${PORT}`)
+})
