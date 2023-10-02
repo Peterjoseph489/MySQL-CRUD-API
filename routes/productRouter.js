029a4f568695cb73ef30e13fdb7882dd97b19ce0
Joseph <josephpeter489@gmail.com>
2023-10-02 15:22:08 +0100

    done

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,13 +1,23 @@
+// Import controllers review, products
 const productController = require('../controllers/productController')
+const reviewController = require('../controllers/reviewController')
 
+// router
 const router = require('express').Router()
 
+
+// Use Routers
 router.post('/addProduct', productController.addProduct)
 router.get('/allProducts', productController.getAllProducts)
 router.get('/:id', productController.getOneProduct)
 router.get('/published', productController.getPublishedProduct)
 router.put('/:id', productController.updateProduct)
 router.delete('/:id', productController.deleteProduct)
+
+
+// Review URL and Controller
+router.post('/addReview', reviewController.addReview)
+router.get('/allReviews', reviewController.getAllReviews)
 
 
 module.exports = router
