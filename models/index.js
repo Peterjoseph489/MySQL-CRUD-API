af93110fe9481d5922dc33df6effb8755be7c457
Joseph <josephpeter489@gmail.com>
2023-10-02 10:18:38 +0100

    my second commit

--- models/index.js
+++ models/index.js
@@ -9,5 +9,28 @@ const sequelize = new Sequelize(
         host: dbConfig.HOST,
         dialect: dbConfig.dialect,
         operatorsAliases: false,
+
+        pool: {
+            max: dbConfig.pool.max,
+            min: dbConfig.pool.min,
+            acquire: dbConfig.pool.acquire,
+            idle: dbConfig.pool.idle,
+        }
     }
-)
+);
+
+sequelize.authenticate()
+,then(()=>{
+    console.log('connected..')
+})
+.catch((err)=>{
+    console.log('Error' + err)
+})
+
+const db = {}
+
+db.Sequelize = Sequelize
+db.sequelize = sequelize
+
+db.products = require('./productModel.js')(sequelize, DataTypes)
+db.reviews = require('./reviewModel.js')(sequelize, DataTypes)
